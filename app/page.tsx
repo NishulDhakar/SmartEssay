6ae63963393c41ec7bbb878faf7b21bb58388e89
Nishul Dhakar <nishuldhakar.se@gmail.com>
2025-06-13 23:12:48 +0530

    build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen items-center justify-center bg-gray-100 text-gray-800">
      Starting soon...
     </div>
   );
